662ac6504f2f6ccef1e022f8920f1e8a3d097013
Grayson Hansard <grayson.hansard@bitrise.io>
2023-04-13 20:25:44 -0400

    Use correct variable name

--- bin/util.sh
+++ bin/util.sh
@@ -6,7 +6,7 @@ symlink_libyaml() {
 		# Ruby doesn't recognize the with-libyaml-dir when loading Psych
 		local libyaml="$(find "$search_path" -name libyaml-0.2.dylib | head -n1)"
 		if [[ ! -e "$libyaml" ]]; then
-			echo "Could not find libyaml-0.2.dylib in $lib_dir!"
+			echo "Could not find libyaml-0.2.dylib in $search_path!"
 			exit 1
 		fi
 		ln -s "$libyaml" "${ruby_src_dir}/libyaml-0.2.dylib"
