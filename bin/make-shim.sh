f44b3de2173334bec54139e50f9524ec7deafcde
Grayson Hansard <grayson.hansard@bitrise.io>
2023-04-14 10:13:09 -0400

    Only copy mkconfig when in Ruby src root

--- bin/make-shim.sh
+++ bin/make-shim.sh
@@ -4,9 +4,12 @@ PARENT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"
 source "${PARENT_DIRECTORY}/util.sh"
 
 main() {
-	symlink_libyaml "." "$(dirname "$PARENT_DIRECTORY")"
-	fix_mkconfig "$PARENT_DIRECTORY" "."
-	make $@
+	# symlink_libyaml "." "$(dirname "$PARENT_DIRECTORY")"
+	if [[ -f ruby.c ]]; then
+		# Only run in Ruby root directory
+		fix_mkconfig "$PARENT_DIRECTORY" "."
+	fi
+	make "$@"
 }
 
 main "$@"
